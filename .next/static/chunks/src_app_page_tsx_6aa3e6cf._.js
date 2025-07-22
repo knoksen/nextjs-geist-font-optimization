4bb2335e3c9a1b66535b786a3135386a0fe586a4
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-07-22 20:47:16 +0200

    Add .next/static/chunks/src_app_page_tsx_6aa3e6cf._.js 1753210034652

---
+++ .next/static/chunks/src_app_page_tsx_6aa3e6cf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_8af98cba._.js",
+  "static/chunks/src_e1d32103._.js"
+],
+    source: "dynamic"
+});
